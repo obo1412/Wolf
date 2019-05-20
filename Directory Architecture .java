eb7c20565d5c2ffcc5a4f934e92affcb7ccf4325
SoowonLee <soo__1@naver.com>
2019-05-20 21:51:51 +0900

    Update Directory Architecture .java

--- Directory Architecture .java
+++ Directory Architecture .java
@@ -3,41 +3,41 @@
  Wolf
  ├── pom.xml (maven 에서 참조하는 설정파일, maven 라이브러리 자동으로 찾아주는 역할)
  └── src (web 디렉토리)
- ├── main
- │   ├── java (java code, 컨트롤러, 모델, 서비스)
- │   │   └── com
- │   │       └── spring
- │   │           ├── helper
- │   │           └── wolf
- │   │               ├── controller
- │   │               │   ├── bbs
- │   │               │   ├── content
- │   │               │   └── member
- │   │               ├── model
- │   │               └── service
- │   │                   └── impl
- │   ├── resources (java code에서 사용할 리소스, mapper, sql)
- │   │   └── mappers (DB 명령어 입력된 xml)
- │   └── webapp (외부에서 접근 가능한 폴더)
- │       ├── WEB-INF (외부에서 직접 접근 불가능, 컴파일된 클래스, 스프링 환경 설정파일, DB 연결정보 등이 존재하기 때문)
- │       │   ├── lib
- │       │   ├── spring (스프링 환경 설정 파일, root-context.xml, servlet-context.xml)
- │       │   │   └── appServlet
- │       │   └── views (html, jsp 파일)
- │       │       ├── bbs
- │       │       ├── content
- │       │       ├── inc
- │       │       ├── member
- │       │       └── test
- │       └── assets  (구글 여러 사이트 참고 시 resources라는 이름을 더 많이 씀, js, css, image 관리)
- │           ├── css
- │           ├── img
- │           ├── js
- │           └── plugins
- │               ├── ajax
- │               ├── ajax-form
- │               ├── handlebars
- │               └── multi-column
+      ├── main
+      │   ├── java (java code, 컨트롤러, 모델, 서비스)
+      │   │   └── com
+      │   │       └── spring
+      │   │           ├── helper
+      │   │           └── wolf
+      │   │               ├── controller
+      │   │               │   ├── bbs
+      │   │               │   ├── content
+      │   │               │   └── member
+      │   │               ├── model
+      │   │               └── service
+      │   │                   └── impl
+      │   ├── resources (java code에서 사용할 리소스, mapper, sql)
+      │   │   └── mappers (DB 명령어 입력된 xml)
+      │   └── webapp (외부에서 접근 가능한 폴더)
+      │       ├── WEB-INF (외부에서 직접 접근 불가능, 컴파일된 클래스, 스프링 환경 설정파일, DB 연결정보 등이 존재하기 때문)
+      │       │   ├── lib
+      │       │   ├── spring (스프링 환경 설정 파일, root-context.xml, servlet-context.xml)
+      │       │   │   └── appServlet
+      │       │   └── views (html, jsp 파일)
+      │       │       ├── bbs
+      │       │       ├── content
+      │       │       ├── inc
+      │       │       ├── member
+      │       │       └── test
+      │       └── assets  (구글 여러 사이트 참고 시 resources라는 이름을 더 많이 씀, js, css, image 관리)
+      │           ├── css
+      │           ├── img
+      │           ├── js
+      │           └── plugins
+      │               ├── ajax
+      │               ├── ajax-form
+      │               ├── handlebars
+      │               └── multi-column
 */
 
 /*
